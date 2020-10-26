ab959d9d2ca360262908f1e4b3976d448e60ad61
Kayalyn Edmeade <kjedmeade@gmail.com>
2020-10-26 09:13:44 -0700

    changes

--- FlowerCrud/src/main/java/com/skilldistillery/jpacrud/FlowerCrudApplication.java
+++ FlowerCrud/src/main/java/com/skilldistillery/jpacrud/FlowerCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlowerCrudApplication {
+public class FlowerCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(FlowerCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlowerCrudApplication.class, args);
